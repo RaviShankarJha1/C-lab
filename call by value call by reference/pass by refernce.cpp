07ff92d64813773fe40f963c48eb81077b6f7e09
RaviShankarJha1 <rjha81467@gmail.com>
2025-08-21 15:50:05 +0530

    Create pass by refernce.cpp

---
+++ call by value call by reference/pass by refernce.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void increment(int &s)
+{
+  s=s+5000;
+  cout<<"salary inside function: "<<s<<endl;
+}
+int main()
+{
+  int sal=27000;
+  increment(sal);
+  cout<<"Salary inside main: "<<sal<<endl;
+  return 0;
+}
